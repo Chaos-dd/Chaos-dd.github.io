77db4a3c9c6f48ef504a466e7b788f502b983ad2
Chaos <ghost03071061@gmail.com>
2016-01-04 00:09:44 +0800

    22

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'https://chaos-dd.github.io'
+SITEURL = 'https://github.com/chaos-dd/chaos-dd.github.io'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
